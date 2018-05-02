fd74ed9df9ee1e2f8f595209b14c53f65d521fcc
Dmitry Bykov <dmitry.bykov@sibers.com>
2018-05-02 18:28:49 +0700

    add validation errors handling

--- EventListener/ResponseEventListener.php
+++ EventListener/ResponseEventListener.php
@@ -1,29 +1,45 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jedi
- * Date: 05.11.17
- * Time: 17:29
- */
 
 namespace Sibers\ApiBundle\EventListener;
 
+use Sibers\ApiBundle\Service\ErrorHandler;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 
-class ResponseEventListener
+final class ResponseEventListener
 {
+    /**
+     * @var array
+     */
+    private static $successStatusCodes = [
+        Response::HTTP_OK,
+        Response::HTTP_CREATED,
+    ];
 
+    /**
+     * @var array
+     */
+    private static $validationFailedStatusCodes = [
+        Response::HTTP_BAD_REQUEST,
+        Response::HTTP_UNAUTHORIZED,
+    ];
+
+    /**
+     * @var ErrorHandler
+     */
     private $errorHandler;
 
     /**
-     * ResponseEventListener constructor.
+     * @param ErrorHandler $errorHandler
      */
-    public function __construct($errorHandler)
+    public function __construct(ErrorHandler $errorHandler)
     {
         $this->errorHandler = $errorHandler;
     }
 
+    /**
+     * @param FilterResponseEvent $event
+     */
     public function onKernelResponse(FilterResponseEvent $event)
     {
         $decoded = json_decode($event->getResponse()->getContent(), true);
@@ -32,22 +48,38 @@ class ResponseEventListener
             return;
         }
 
-        if (
-            $event->getResponse()->getStatusCode() === Response::HTTP_OK
-            || $event->getResponse()->getStatusCode() === Response::HTTP_CREATED
-        ) {
-            $result['status'] = 'success';
-            $result['response'] = $decoded;
+        $statusCode = $event->getResponse()->getStatusCode();
+
+        if (in_array($statusCode, self::$successStatusCodes)) {
+            $result = [
+                'status'   => 'success',
+                'response' => $decoded,
+            ];
             $event->getResponse()->setContent(json_encode($result));
         } elseif (isset($decoded['status']) && $decoded['status'] == 'error') {
             return;
+        } elseif (
+            in_array($statusCode, self::$validationFailedStatusCodes)
+            && !empty($decoded['violations'])
+        ) {
+            $errors = array_map(function (array $violation) {
+                return [
+                    'code'        => isset($violation['code']) ? $violation['code'] : null,
+                    'name'        => $violation['propertyPath'],
+                    'description' => $violation['message'],
+                ];
+            }, $decoded['violations']);
+
+            $result = [
+                'status' => 'error',
+                'errors' => $errors
+            ];
+            $event->getResponse()->setContent(json_encode($result));
         } else {
            $statusCode = $event->getResponse()->getStatusCode();
            $message = isset($decoded['message']) ? $decoded['message'] : '';
 
-           $event->setResponse($this->errorHandler->getResponse($statusCode, -1, $message, 'Internal eror'));
+           $event->setResponse($this->errorHandler->getResponse($statusCode, -1, $message, 'Internal error'));
         }
-
-
     }
 }
